330d3b6420831b34df041d44a244f0f41d2b2ecf
Saqibur <saqibur.rahman98@gmail.com>
2020-07-11 20:11:19 +0600

    Add redis settings

--- GPWorkflow/settings.py
+++ GPWorkflow/settings.py
@@ -123,6 +123,13 @@ USE_L10N = True
 USE_TZ = False
 
 
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'Asia/Dhaka'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
